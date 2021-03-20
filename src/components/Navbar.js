3637a04358d290d4e9d452b538b77457c5864a35
neelam618 <you@example.com>
2021-03-20 11:43:33 +0530

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import LogoInverted from './logo-inverted.png'
 
 function Navbar() {
     return (
-        <header className="bg-gray-900">
-            <div className="flex items-center justify-between px-4 py-3">
+        <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+            <div className="flex items-center justify-between px-4 py-3 sm:p-0">
                 <div>
                     <img className="h-8" src={LogoInverted} alt="Workcation"/>
                 </div>
-                <div>
+                <div className="sm:hidden">
                     <button type="button" className="text-gray-500 focus:text-white focus:outline-none hover:text-white">
                         <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
@@ -15,10 +15,10 @@ function Navbar() {
                     </button>
                 </div>
             </div>
-            <div className="px-2 pt-2 pb-4">
-                <a className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800" href="#">List your property</a>
-                <a className="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800" href="#">Trips</a>
-                <a className="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800" href="#">Messages</a> 
+            <div className="px-2 pt-2 pb-4 sm:flex sm:p-0">
+                <a className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2" href="#">List your property</a>
+                <a className="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2" href="#">Trips</a>
+                <a className="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2" href="#">Messages</a> 
             </div>
         </header>
     )
