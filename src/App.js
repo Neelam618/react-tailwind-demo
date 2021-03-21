cef5939c7ea06fd3009bf8e26752a41b09d754f6
neelam618 <you@example.com>
2021-03-21 10:08:05 +0530

    Destination card

--- src/App.js
+++ src/App.js
@@ -6,20 +6,48 @@ import DestinationCard from './components/DestinationCard';
 
 
 function App() {
-let destination = [
+let destinations = [
   {
     city: 'Toronto',
     averagePrice: 120,
     propertyCount: 76,
-    imageUrl: '/img/toronto.jpg',
+    imageUrl: './img/toronto.jpg',
     imageAlt: 'Toronto skyline',
   },
   {
     city: 'Malibu',
     averagePrice: 215,
     propertyCount: 43,
-    imageUrl: '/img/malibu.jpg',
+    imageUrl: './img/malibu.jpg',
     imageAlt: 'Cliff in Malibu',
+  },
+  {
+    city: 'Chicago',
+    averagePrice: 130,
+    propertyCount: 115,
+    imageUrl: './img/chicago.jpg',
+    imageAlt: 'Chicago skyline',
+  },
+  {
+    city: 'Seattle',
+    averagePrice: 135,
+    propertyCount: 63,
+    imageUrl: 'img/seattle.jpg',
+    imageAlt: 'Seattle skyline',
+  },
+  {
+    city: 'Colorado',
+    averagePrice: 85,
+    propertyCount: 47,
+    imageUrl: './img/colorado.jpg',
+    imageAlt: 'Lake in Colorado',
+  },
+  {
+    city: 'Miami',
+    averagePrice: 115,
+    propertyCount: 86,
+    imageUrl: './img/miami.jpg',
+    imageAlt: 'Beach in Miami',
   }
 ]
 
@@ -45,20 +73,20 @@ let destination = [
             <img className="absolute inset-0 w-full h-full object-cover object-center" src={MainImage} alt="Woman workcationing on the beach" />
           </div>    
         </div>
-
         <div>
           <div className="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
             <h2 className="text-xl text-gray-900">Popular destinations</h2>
             <p className="text-gray-600">A selection of great work-friendly cities with lots to see and explore.</p>
             <div className="flex flex-wrap -mx-4">
-              <div className="mt-6 w-full px-4 lg:w-1/2 xl:w-1/3">
-                <DestinationCard destination={destination}/>
-              </div>
+              {destinations.map(destinationItem => {
+                return <div className="mt-6 w-full px-4 lg:w-1/2 xl:w-1/3">
+                  <DestinationCard destination={destinationItem} />
+                </div>
+              })}
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 }
