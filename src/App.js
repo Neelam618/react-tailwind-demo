fb466fbff1b9916974ac87d87040d74aa4a87a7d
neelam618 <you@example.com>
2021-03-21 11:40:50 +0530

    close dropdown when clicked anywhere on screen

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ let destinations = [
             </div>
           </div>
           <div className="hidden lg:block lg:w-1/2 lg:relative">
-            <img className="absolute inset-0 w-full h-full object-cover object-center" src={MainImage} alt="Woman workcationing on the beach" />
+            <img className="absolute z-0 inset-0 w-full h-full object-cover object-center" src={MainImage} alt="Woman workcationing on the beach" />
           </div>    
         </div>
         <div>
--- src/components/AccountDropdown.js
+++ src/components/AccountDropdown.js
@@ -5,9 +5,12 @@ function AccountDropdown() {
 
     return (
         <div className="relative">
-            <button onClick={() => setShowDropdown(!showDropdown)} className="block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:border-white focus:outline-none">
+            <button onClick={() => setShowDropdown(!showDropdown)} className="block relative z-50 h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:border-white focus:outline-none">
                 <img className="h-full w-full object-cover" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar" />
             </button>
+            {showDropdown ? (
+                <button onClick={() => setShowDropdown(false)} tabIndex="-1" className="fixed z-10 inset-0 bg-black opacity-50 cursor-default h-full w-full"></button>
+            ) : null}
             {showDropdown ? (
                  <div className="absolute right-0 z-10 w-48 mt-2 py-2 bg-white rounded-lg shadow-xl">
                  <a className="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white" href="#">Account settings</a>
